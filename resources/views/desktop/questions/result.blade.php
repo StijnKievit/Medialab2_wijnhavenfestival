a4e67953ee9b7c4b0bf28334a73b313ca5aa9869
Stijn Kievit <0875013@hr.nl>
2016-06-15 17:37:14 +0200

    fix for large screens

--- resources/views/desktop/questions/result.blade.php
+++ resources/views/desktop/questions/result.blade.php
@@ -19,51 +19,52 @@
                     <a href="{{url('/question')}}" class="redo-test-btn">Doe de test opnieuw</a>
                 </div>
             </div>
-            <div class="grid-1-2 no-pad" id="sub_content">
+            <div class="grid-1-2 no-pad b-background" id="sub_content" style="overflow-y: hidden">
                 <div class="padding_bar d-background">
 
                 </div>
+                <div class="sub_content_content_box" style="overflow-y: scroll; height: 100%;">
+                    <div class="app_segment b-background main_content">
 
-                <div class="app_segment b-background main_content">
+                        <p class=" basic">
+                            {!! $zeebonk[0]['beschrijving'] !!}
+                        </p>
+                        <p class=" basic">
+                            {!! $zeebonk[0]['beschrijving_eten'] !!}
+                        </p>
+                        <p class=" basic">
+                            Hiervoor kun je terecht bij de onderstaande horeca:
+                        </p>
+                    </div>
+                    <div class="app_segment no-marg b-background row">
+                        @foreach($horeca as $item)
+                            <div class="horeca_block">
+                                <img class="horeca_img" src="{{URL::asset($item->afbeelding)}}" alt="">
+                                <a class="horeca_button" href="{{url('map/'.$item->id)}}">
+                                    {{$item->naam}}
 
-                    <p class=" basic">
-                        {!! $zeebonk[0]['beschrijving'] !!}
-                    </p>
-                    <p class=" basic">
-                        {!! $zeebonk[0]['beschrijving_eten'] !!}
-                    </p>
-                    <p class=" basic">
-                        Hiervoor kun je terecht bij de onderstaande horeca:
-                    </p>
-                </div>
-                <div class="app_segment no-marg b-background">
-                    @foreach($horeca as $item)
-                        <div class="horeca_block">
-                            <img class="horeca_img" src="{{URL::asset($item->afbeelding)}}" alt="">
-                            <a class="horeca_button" href="{{url('map/'.$item->id)}}">
-                                {{$item->naam}}
-
-                            <img class="horeca_arrow desktop_horeca_arrow" src="{{URL::asset('css/img/Pijltje-04.png')}}">
+                                    <img class="horeca_arrow desktop_horeca_arrow" src="{{URL::asset('css/img/Pijltje-04.png')}}">
                                 </a>
-                        </div>
-                    @endforeach
-                    {{--<div class="horeca_block">--}}
+                            </div>
+                        @endforeach
+                        {{--<div class="horeca_block">--}}
                         {{--<img class="horeca_img" src="http://jouzeebonk.dev/img/Hanger.png" alt="">--}}
                         {{--<a class="horeca_button" href="http://jouzeebonk.dev/map/2">--}}
-                            {{--Hangar 85--}}
+                        {{--Hangar 85--}}
                         {{--</a>--}}
                         {{--<img class="horeca_arrow" src="http://jouzeebonk.dev/css/img/Pijltje-04.png">--}}
-                    {{--</div>--}}
+                        {{--</div>--}}
 
+                    </div>
+                    <div class="buffer" style="height: 40px"></div>
                 </div>
+
+
 
 
             </div>
         </div>
     </div>
-    <script type="application/javascript">
-    </script>
-
     <script type="application/javascript">
 
         var padding_bar = $(".padding_bar");
@@ -105,6 +106,10 @@
           setSubContent("#sub_content");
           adjustSubContent();
 
+      });
+
+      $( window ).resize(function() {
+          adjustSubContent();
       });
   </script>
 
